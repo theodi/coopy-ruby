8457c7f8b5d860a287220c52e147088a2bba2098
James Smith <james@floppy.org.uk>
2013-11-12 18:01:32 +0000

    add pending test for table diff

---
+++ spec/integration/table_diff_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe "diffing tables" do
+
+  {
+    "planetary_bodies.csv" => "planetary_bodies.html"
+  }.each_pair do |input, output|
+
+    it "should generate #{output} from #{input} when generating HTML" do
+
+      pending
+
+    end
+    
+  end
+end
