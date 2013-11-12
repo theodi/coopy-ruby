e129b150692dedf1c452854e59b2421aaea4eea7
James Smith <james@floppy.org.uk>
2013-11-12 13:07:29 +0000

    convert ChangeType

---
+++ lib/coopy/change_type.rb
@@ -0,0 +1,10 @@
+module Coopy
+  module ChangeType
+    NO_CHANGE     = :no_change
+    REMOTE_CHANGE = :remote_change
+    LOCAL_CHANGE  = :local_change
+    BOTH_CHANGE   = :both_change
+    SAME_CHANGE   = :same_change
+    NOTE_CHANGE   = :note_change
+  end
+end
