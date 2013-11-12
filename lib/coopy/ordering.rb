5f4c8693a588f6a70c1dbda44ce7688435ef9cc7
James Smith <james@floppy.org.uk>
2013-11-12 16:19:02 +0000

    convert Ordering

---
+++ lib/coopy/ordering.rb
@@ -0,0 +1,27 @@
+module Coopy
+  class Ordering
+
+    def initialize
+      @order = []
+      @ignore_parent = false
+    end
+
+    def add(l, r, p = -2)
+      p = -2 if @ignore_parent
+      @order << Coopy::Unit.new(l,r,p)
+    end
+
+    def get_list
+      @order
+    end
+
+    def to_string
+      @order.map{|x| x.to_string}.join(", ")
+    end
+
+    def ignore_parent
+      @ignore_parent = true;
+    end
+
+  end
+end
---
+++ spec/libs/ordering_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe Coopy::Ordering do
+  
+  it "should convert order to string correctly" do
+    # test default behaviour
+    o = Coopy::Ordering.new
+    o.to_string.should == ''
+    # add a couple of units
+    o.add 1,2,3
+    o.add 3,2,1
+    o.to_string.should == "3|1:2, 1|3:2"
+  end
+
+  it "should not ignore parent if not set" do
+    o = Coopy::Ordering.new
+    o.add 1,2,3
+    o.to_string.should == "3|1:2"
+  end
+
+  it "should ignore parent if set" do
+    o = Coopy::Ordering.new
+    o.ignore_parent
+    o.add 1,2,3
+    o.to_string.should == "1:2"
+  end
+
+end
